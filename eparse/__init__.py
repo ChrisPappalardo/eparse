bf09877c1442262dc28130ef4a25fd68941f5055
Chris Pappalardo <cpappala@gmail.com>
2023-06-07 18:23:02 -0700

    bumped version for pypi release

--- eparse/__init__.py
+++ eparse/__init__.py
@@ -1,5 +1,7 @@
-"""Top-level package for eparse."""
+'''
+Top-level package for eparse.
+'''
 
-__author__ = """Chris Pappalardo"""
+__author__ = 'Chris Pappalardo'
 __email__ = 'cpappala@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'

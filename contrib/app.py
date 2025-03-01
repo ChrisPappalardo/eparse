10bacd5c4f5aeddcd3b68314578008da97c23f08
Chris Pappalardo <cpappala@gmail.com>
2025-03-01 08:37:08 -0800

    update gradio app for basic functionality

--- contrib/app.py
+++ contrib/app.py
@@ -1,11 +1,23 @@
+from typing import Any, Optional
+
 import gradio as gr
 import pandas as pd
+
+from eparse.core import get_df_from_file
 
 
 # create context
 class Context:
     def __init__(self) -> None:
-        self.excel_file = None
+        self.selected_file: Optional[gr.File] = None
+        self.selected_sheet: Optional[str] = None
+        self.selected_table: Optional[str] = None
+        # df, excel_RC, name, sheet
+        self._tables = list[tuple[pd.DataFrame, str, str, str]]
+
+    def __str__(self) -> str:
+        v = (self.selected_file, self.selected_sheet, self.selected_table)
+        return "{} {} {}".format(*v)
 
 
 context = Context()
@@ -13,13 +25,35 @@ context = Context()
 
 # create gradio functions
 def render_table(
-    excel_file: str,
+    selected_file: str,
     selected_sheet: str,
     selected_table: str,
     context: Context = context,
-) -> pd.DataFrame:
+) -> tuple[str, str, pd.DataFrame]:
     """loads and renders table from file and sheet"""
-    return pd.DataFrame()
+
+    if selected_file != context.excel_file:
+        context.selected_file = selected_file
+        context.selected_sheet = selected_sheet
+        context.selected_table = selected_table
+        context._tables = list()
+        for df, excel_RC, name, sheet in get_df_from_file(excel_file):
+            context._tables.append((df, excel_RC, name, sheet))
+            context.selected_sheet = sheet
+            context.selected_table = name
+
+    elif selected_sheet != context.selected_sheet:
+        context.selected_sheet = selected_sheet
+        context.selected_table = None
+
+    elif selected_table != context.selected_table:
+        context.selected_table = selected_table
+
+    for df, excel_RC, name, sheet in context._tables:
+        if sheet == selected_sheet and name == selected_table:
+            return (excel_RC, name, df)
+
+    return ("", "", pd.DataFrame())
 
 
 with gr.Blocks(
